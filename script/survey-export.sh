2a3fa4626fb3965b87829bd6367913690752f862
w <w@ws-iMac.local>
2018-02-11 20:15:58 +0800

    fix bug

--- script/survey-export.sh
+++ script/survey-export.sh
@@ -2,6 +2,6 @@
 csv_dir=/opt/mysql/backups
 csv_file=${csv_dir}/survey.txt
 dest=/usr/share/nginx/survey/survey-1b3df2349a.csv
-mysqldump -uroot -p123456 -t --tab=${csv_dir} --fields-terminated-by=, --fields-enclosed-by='"' --lines-terminated-by=0x0d0a ngo20map survey > ${csv}
+mysqldump -uroot -p123456 -t --tab=${csv_dir} --fields-terminated-by=, --fields-enclosed-by='"' --lines-terminated-by=0x0d0a ngo20map survey
 rm $(dest)
 iconv -f utf-8 -t utf-16 ${csv_file} > ${dest}
