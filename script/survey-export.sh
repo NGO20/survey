ee6337ce4a6aab99cb7d7ba7d2a9c3733606173a
w <w@ws-iMac.local>
2018-02-11 20:12:09 +0800

    fix script errors

--- script/survey-export.sh
+++ script/survey-export.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env sh
-csv=/opt/mysql/backups/survey.csv
+csv_dir=/opt/mysql/backups/
+csv_file=${csv_dir}/survey.txt
 dest=/usr/share/nginx/survey/survey-1b3df2349a.csv
-mysqldump -uroot -p123456 --tab=/tmp --fields-terminated-by=, --fields-enclosed-by='"' --lines-terminated-by=0x0d0a ngo20map survey > ${csv}
+mysqldump -uroot -p123456 -t --tab=${csv_dir} --fields-terminated-by=, --fields-enclosed-by='"' --lines-terminated-by=0x0d0a ngo20map survey > ${csv}
 rm $(dest)
-iconv -f utf-8 -t utf-16 ${csv} > ${dest}
+iconv -f utf-8 -t utf-16 ${csv_file} > ${dest}
