b199936f2e06df92853d24276b968fb00e359d01
w <w@ws-iMac.local>
2018-02-12 14:38:48 +0800

    refactoring survey result page, change csv encoding to gbk

--- script/survey-export.sh
+++ script/survey-export.sh
@@ -4,4 +4,4 @@ csv_file=${csv_dir}/survey.txt
 dest=/usr/share/nginx/survey/survey-1b3df2349a.csv
 mysqldump -uroot -p123456 -t --tab=${csv_dir} --fields-terminated-by=, --fields-enclosed-by='"' --lines-terminated-by=0x0d0a ngo20map survey
 rm $(dest)
-iconv -f utf-8 -t utf-16 ${csv_file} > ${dest}
+iconv -f utf-8 -t gbk ${csv_file} > ${dest}
